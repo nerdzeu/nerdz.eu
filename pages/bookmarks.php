7c6d6eee0b59d5cddc8574f4d9d9bb2b118a7bc5
Paolo Galeone <nessuno@nerdz.eu>
2013-05-22 19:31:51 +0200

    Bookmarks senza codice ripetuto e senza controlli ripetuti

--- pages/bookmarks.php
+++ pages/bookmarks.php
@@ -39,6 +39,9 @@ if($prj)
 				"SELECT groups_bookmarks.hpid, groups_bookmarks.time, groups_posts.message, groups_posts.to, groups_posts.pid FROM `groups_bookmarks` INNER JOIN `groups_posts` ON groups_posts.hpid = groups_bookmarks.hpid WHERE groups_bookmarks.from = ? AND {$orderby} LIKE ? ORDER BY {$orderby} {$order} LIMIT {$limit}",
 				array($_SESSION['nerdz_id'],"%{$q}%")
 			 );
+
+	$linkMethod = 'projectLink';
+	$nameMethod = 'getProjectName';
 }
 else
 {
@@ -54,6 +57,9 @@ else
 				"SELECT bookmarks.hpid, bookmarks.time, posts.message, posts.to, posts.pid FROM `bookmarks` INNER JOIN `posts` ON posts.hpid = bookmarks.hpid WHERE bookmarks.from = ? AND {$orderby} LIKE ? ORDER BY {$orderby} {$order} LIMIT {$limit}",
 				array($_SESSION['nerdz_id'],"%{$q}%")
 			 );
+
+	$linkMethod = 'userLink';
+	$nameMethod = 'getUserName';
 }
 
 $vals['list_a'] = array();
@@ -68,9 +74,9 @@ if(($r = $core->query($query,db::FETCH_STMT)))
         $vals['list_a'][$i]['datetime_n'] = $core->getDateTime($o->time);
         $vals['list_a'][$i]['pid_n'] = $o->pid;
         $vals['list_a'][$i]['hpid_n'] = $o->hpid;
-        $vals['list_a'][$i]['name_n'] = ($prj) ? $core->getProjectName($o->to) : $core->getUserName($o->to);
+        $vals['list_a'][$i]['name_n'] = $core->$nameMethod($o->to);
         $vals['list_a'][$i]['preview_n'] = $core->bbcode(htmlentities(substr(html_entity_decode($o->message,ENT_QUOTES,'UTF-8'),0,256),ENT_QUOTES,'UTF-8').'...',true);
-        $vals['list_a'][$i]['link_n'] = ($prj) ? '/'.phpCore::projectLink($vals['list_a'][$i]['name_n']).$o->pid : '/'.phpCore::userLink($vals['list_a'][$i]['name_n']).$o->pid;
+        $vals['list_a'][$i]['link_n'] = phpCore::$linkMethod($vals['list_a'][$i]['name_n']).$o->pid;
 
         ++$i;
     }
