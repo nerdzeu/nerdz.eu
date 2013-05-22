d282e53db8aec3d08ed67844efee4fff3e3e9e14
Paolo Galeone <nessuno@nerdz.eu>
2013-05-22 19:42:43 +0200

    Bookmarks fixed broken link

--- pages/bookmarks.php
+++ pages/bookmarks.php
@@ -76,7 +76,7 @@ if(($r = $core->query($query,db::FETCH_STMT)))
         $vals['list_a'][$i]['hpid_n'] = $o->hpid;
         $vals['list_a'][$i]['name_n'] = $core->$nameMethod($o->to);
         $vals['list_a'][$i]['preview_n'] = $core->bbcode(htmlentities(substr(html_entity_decode($o->message,ENT_QUOTES,'UTF-8'),0,256),ENT_QUOTES,'UTF-8').'...',true);
-        $vals['list_a'][$i]['link_n'] = phpCore::$linkMethod($vals['list_a'][$i]['name_n']).$o->pid;
+        $vals['list_a'][$i]['link_n'] = '/'.phpCore::$linkMethod($vals['list_a'][$i]['name_n']).$o->pid;
 
         ++$i;
     }
