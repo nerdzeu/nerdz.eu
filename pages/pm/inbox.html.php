95444f29df1420829865e1180ff381b153158363
Paolo Galeone <nessuno@nerdz.eu>
2013-08-16 12:55:34 +0200

    Security issue fixed

--- pages/pm/inbox.html.php
+++ pages/pm/inbox.html.php
@@ -4,7 +4,10 @@ ob_start('ob_gzhandler');
 require_once $_SERVER['DOCUMENT_ROOT'].'/class/pm.class.php';
 $core = new pm();
 ob_start(array('phpCore','minifyHtml'));
-	
+
+if(!$core->isLogged())
+	die($core->lang('REGISTER'));
+
 $vals = array();
 $vals['from'] = $core->lang('FROM');
 $vals['read'] = $core->lang('READ');
