f84917c99ee6e62f8efc6bf343da0ed08aec735e
mcilloni <krnlpk@gmail.com>
2013-09-23 22:46:30 +0200

    Added fastfetch

---
+++ class/fastfetch.class.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/class/pm.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/class/fastfetcherror.class.php';
+
+/**
+    FastFetch is a class for fast pm fetching - and maybe more, someday.
+*/
+final class FastFetch {
+
+    private $mPm;
+
+    public function __construct() {
+
+        $this->mPm = new pm();
+
+    }
+
+    /**
+     * Returns true if current session is associated with a logged user.
+     * @return boolean 
+     */
+    public function isLogged() {
+        return $this->mPm->isLogged();
+    }
+
+    /**
+     * Returns all conversations.
+     * 
+     * @return array an object containing the list of conversations.
+     * @throws FFException if something wrong happens
+     */
+    public function fetchConversations() {
+
+        $ret = new stdClass();
+
+        $list = $this->mPm->getList();
+        
+        if($list !== NULL) {
+            
+            $ret = array();
+
+            foreach($list as $conversation) {
+                
+                $element = array(
+                            'name' => $conversation['from_n'],
+                            'last_timestamp' => $conversation['timestamp_n'],
+                            'id' => $conversation['fromid_n'],
+                            'last_message' => html_entity_decode($this->mPm->getLastMessageForConversation((int) $conversation['fromid_n']), ENT_QUOTES)                        
+                            );
+
+                $ret[] = $element;
+
+            }
+
+        } else {
+            throw new FFException(FFErrCode::SERVER_FAILURE);
+        }
+
+        return $ret;
+
+    }
+
+    /**
+     * Fetches $limit messages starting with the $start-th in the conversation with user $otherId.
+     * 
+     * @param int $otherId 
+     * @param int $start
+     * @param int $limit
+     * @return array an array of objects representing 
+     * @throws FFException if something wrong happens
+     */
+    public function fetchMessages($otherId, $start = 0, $limit = 10) {
+    
+        if ($limit > 30) {
+            throw new FFException(FFErrCode::LIMIT_EXCEEDED);
+        }
+        
+        $me = $this->mPm->getUserId();
+        
+        $list = $this->mPm->query (
+            [
+                'SELECT ("from" = :me) AS SENT, EXTRACT(EPOCH FROM time) AS timestamp, message, read FROM PMS WHERE ("from" = :from1 AND "to" = :to1) OR ("to" = :to2 AND "from" = :from2) ORDER BY TIME DESC LIMIT '.$limit.' OFFSET '.$start, 
+                [
+                    ':me' => $me,
+                    ':from1' => $otherId,
+                    ':to1' => $me,
+                    ':from2' => $me,
+                    ':to2' => $otherId
+                ]
+            ],
+            db::FETCH_OBJ,
+            true 
+        );
+        
+        if ($list === NULL) {
+           throw new FFException(FFErrCode::SERVER_FAILURE);
+        }
+        
+        foreach ($list as $row) {
+            $row->message = html_entity_decode($row->message, ENT_QUOTES);
+        }
+        
+        return $list;
+
+    }
+
+}
+
+?>
---
+++ fastfetch.json.php
@@ -0,0 +1,80 @@
+<?php
+
+ob_start('ob_gzhandler');
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/class/fastfetch.class.php';
+
+function jsonResponse($object) {
+    header('Content-Type: application/json; charset=utf-8');
+    exit(json_encode($object, JSON_UNESCAPED_UNICODE));
+}
+
+function bakeError($exception) {
+    
+    $code = FFErrCode::UNKNOWN;
+    
+    if ($exception instanceof FFException) {
+        $code = $exception->code;
+    }
+        
+    return ['error' => $code];
+}
+
+
+
+$ff = new FastFetch();
+try {
+    
+    if(!$ff->isLogged()) {
+        throw new FFException(FFErrCode::NOT_LOGGED);
+    }
+
+    if(!isset($_GET['action'])) {
+        throw new FFException(FFErrCode::NO_ACTION);
+    }
+    
+    $response = NULL;
+
+    switch($_GET['action']) {    
+
+        case 'conversations': 
+            $response = $ff->fetchConversations();
+            break;
+
+        case 'messages': {
+
+            if (!isset($_GET['otherid']) || !is_numeric($_GET['otherid'])) {
+                throw new FFException(FFErrCode::NO_OTHER_ID);
+            }
+
+            $start = 0;
+            $limit = 10;
+
+            if (isset($_GET['start']) ^ isset($_GET['limit'])) {
+                throw new FFException(FFErrCode::WRONG_REQUEST);
+            }
+
+            if (isset($_GET['start']) && isset($_GET['limit'])) {
+                $start = $_GET['start'];
+                $limit = $_GET['limit'];
+                if(!is_numeric($start) || !is_numeric($limit)) {
+                    throw new FFException(FFErrCode::WRONG_REQUEST);
+                }
+            }
+
+            $response = $ff->fetchMessages($_GET['otherid'], $start, $limit);
+
+            break;
+        }
+        default:
+            throw new FFException(FFErrCode::INVALID_ACTION);
+
+    }
+} catch (FFException $e) {
+    $response = bakeError($e);
+}
+
+jsonResponse($response);
+
+?>
+ 
