d77fdf035c71ecd7ae8c9525502925ef4ba99d88
Dr.Jest <dc.jest@gmail.com>
2013-10-24 16:03:48 +0200

    Immagini in /tpl/1/preferences/images

--- pages/preferences/main.php
+++ pages/preferences/main.php
@@ -11,7 +11,7 @@ $vals['language'] = $core->lang('LANGUAGE');
 $vals['delete'] = $core->lang('DELETE');
 $vals['description_n'] = $core->lang('PREFERENCES_DESCR');
 
-$vals['user_menu_m']= ('<div class="title">'.$core->lang('USER_MENU').'</div><div class="box_menu"> <ul><a href="/"><li><img src="tpl/1/base/images/home-dark.png">Home</li></a><a href="/'.phpCore::userLink($core->getUserName()).'"><li><img src="tpl/1/base/images/prof.png">'.$core->lang('PROFILE').'</li></a><a href="/preferences.php"><li><img src="tpl/1/base/images/settings.png">'.$core->lang('PREFERENCES').'</li></a><a href="/" id="logout" data-tok="'.$core->getCsrfToken().'"><li><img src="tpl/1/base/images/exit.png">'.$core->lang('LOGOUT').'</li></a></ul></div>');
+$vals['usermenu']= $core->lang('USER_MENU');
 
 $core->getTPL()->assign($vals);
 $core->getTPL()->draw('preferences/layout');
