773842a224acebb6f0ed160ccec30800af43ea69
Paolo Galeone <nessuno@nerdz.eu>
2013-12-21 19:37:44 +0100

    Removed some shits

--- pages/informations.php
+++ pages/informations.php
@@ -17,7 +17,7 @@ $exp = explode("\n",$txt);
 $vals = array();
 $vals['informations_n'] = $txt;
 
-$vals['user_menu_m']= ('<div class="title">'.$core->lang('USER_MENU').'</div><div class="box_menu"> <ul><a href="/"><li><img src="tpl/1/base/images/home-dark.png">Home</li></a><a href="/'.phpCore::userLink($core->getUserName()).'"><li><img src="tpl/1/base/images/prof.png">'.$core->lang('PROFILE').'</li></a><a href="/preferences.php"><li><img src="tpl/1/base/images/settings.png">'.$core->lang('PREFERENCES').'</li></a><a href="/" id="logout" data-tok="'.$core->getCsrfToken().'"><li><img src="tpl/1/base/images/exit.png">'.$core->lang('LOGOUT').'</li></a></ul></div>');
+require_once $_SERVER['DOCUMENT_ROOT'].'/pages/common/mobilemenu.php';
 
 $core->getTPL()->assign($vals);
 $core->getTPL()->draw('base/informations');
