a58400916691c23c76233594d75ba9e3e6bc3498
DrJest <dc.jest@gmail.com>
2014-02-14 00:47:24 +0100

    Reviewed Mobile Splashscreen

--- pages/splash.php
+++ pages/splash.php
@@ -1,15 +1,10 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/pages/common/vars.php';
+$vals["mobile_b"] = $core->isMobile();
 if(!$core->isMobile()) {
-  $vals["splash_n"] = "NERDZ ha anche una versione mobile, vuoi provarla?";
-  $vals["yessa_n"] = "Vai alla versione mobile";
-  $vals["nope_n"] = "Rimani sul sito desktop";
   $vals["yes_link_n"] = "mobile";
   $vals["nop_link_n"] = "desktop";
 } else {
-  $vals["splash_n"] = "Vuoi passare alla versione desktop?";
-  $vals["yessa_n"] = "Si";
-  $vals["nope_n"] = "No";
   $vals["yes_link_n"] = "desktop";
   $vals["nop_link_n"] = "mobile";
 }
--- splash.php
+++ splash.php
@@ -1,19 +1,26 @@
 <?php
     $referer = $_SESSION['referer'];
-    if(isset($_REQUEST["mobile"]))
+    if(isset($_GET["mobile"]))
     {
       unset($_SESSION["referer"]);
       setcookie("mobile-splash","mobile",2000000000,"/","nerdz.eu",false,true);
-      header("location: http://mobile.nerdz.eu".$referer);
+      header("Location: http://mobile.nerdz.eu".$referer);
+      die();
     }
-    if(isset($_REQUEST["desktop"]))
+    if(isset($_GET["desktop"]))
     {
       unset($_SESSION["referer"]);
       setcookie("mobile-splash","desktop",2000000000,"/","nerdz.eu",false,true);
       if(!$referer||$referer==$_SERVER["PHP_SELF"])
-        header("location: /");
+      {
+        header("Location: /");
+        die();
+      }
       else
-        header("location: /".$referer);
+      {
+        header("Location: /".$referer);
+        die();
+      }
     }
     ob_start('ob_gzhandler');
     require_once $_SERVER['DOCUMENT_ROOT'].'/class/core.class.php';
@@ -32,7 +39,6 @@
         <meta name="keywords" content="nerdz, social network, user profile, paste, source code, programming" />
         <meta name="description" content="NERDZ is a mix between a social network and a forum. You can share your code, enjoy information technology, talk about nerd stuff and more. Join in!" />
         <meta name="robots" content="index,follow" />
-        <meta name="google-site-verification" content="dRirpMHbSmUiPDrNohR5kmUyUnrii1fkWmTADXmksQY" />
         <title><?= $core->getSiteName(); ?> - Mobile Redirect</title>
 <?php
     $headers = $tplcfg->getTemplateVars('index');
