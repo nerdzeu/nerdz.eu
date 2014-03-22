72ed97143e4ca6a07bd6e6e5b991c262ade99da9
Roberto Frenna <roberto.frenna@gmail.com>
2014-03-22 16:10:37 +0100

    Removed the colon from the nick changed notice
    Many users reported that the colon is not necessary, so I removed it.

--- pages/preferences/account.html.json.php
+++ pages/preferences/account.html.json.php
@@ -209,7 +209,7 @@ if($control)
 if($usernamechanged)
 {
     $lastpid = $core->countMessages(USERS_NEWS) + 1;
-    $message = "{$obj->username} %%12now is34%%: [user]{$user['username']}[/user].";
+    $message = "{$obj->username} %%12now is34%% [user]{$user['username']}[/user].";
 
     if(db::NO_ERR != $core->query(array('INSERT INTO "posts" ("from","to","pid","message","notify", "time") VALUES ('.USERS_NEWS.','.USERS_NEWS.",{$lastpid}, :msg , FALSE, NOW())",array($message)),db::FETCH_ERR))
         die($core->jsonResponse('error',$core->lang('ERROR')));
