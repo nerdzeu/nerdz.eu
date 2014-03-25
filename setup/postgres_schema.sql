bb28f9b5cc7ece3b9998f43a8adbbcf10b68400e
Dr.Jest <dc.jest@gmail.com>
2014-03-25 02:11:45 +0100

    Added comment_thumbs to nerdz_schema, update submodules tpl/0 tpl/2

--- setup/postgres_schema.sql
+++ setup/postgres_schema.sql
@@ -107,6 +107,16 @@ CREATE TABLE thumbs (
   CONSTRAINT chkVote CHECK("vote" IN (-1, 0, 1))
 );
 
+CREATE TABLE comment_thumbs (
+  "hcid" int8 NOT NULL,
+  "user" int8 NOT NULL,
+  "vote" int2 NOT NULL,
+  PRIMARY KEY("hcid", "user"),
+  CONSTRAINT hcidThumbs FOREIGN KEY ("hcid") REFERENCES comments(hcid) ON DELETE CASCADE,
+  CONSTRAINT userThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
+  CONSTRAINT chkVote CHECK("vote" IN (-1, 0, 1))
+);
+
 CREATE TABLE lurkers (
   "user" int8 NOT NULL,
   post int8 NOT NULL,
@@ -303,6 +313,16 @@ CREATE TABLE groups_thumbs (
   "vote" int2 NOT NULL,
   PRIMARY KEY("hpid", "user"),
   CONSTRAINT hpidGThumbs FOREIGN KEY ("hpid") REFERENCES groups_posts(hpid) ON DELETE CASCADE,
+  CONSTRAINT userGThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
+  CONSTRAINT chkGVote CHECK("vote" IN (-1, 0, 1))
+);
+
+CREATE TABLE comment_groups_thumbs (
+  "hcid" int8 NOT NULL,
+  "user" int8 NOT NULL,
+  "vote" int2 NOT NULL,
+  PRIMARY KEY("hcid", "user"),
+  CONSTRAINT hcidGThumbs FOREIGN KEY ("hcid") REFERENCES groups_comments(hcid) ON DELETE CASCADE,
   CONSTRAINT userGThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
   CONSTRAINT chkGVote CHECK("vote" IN (-1, 0, 1))
 );
