3e77dfd6b5e1e860095bd74efde543514ef370ca
Paolo Galeone <nessuno@nerdz.eu>
2014-03-25 09:49:41 +0100

    Quick fixes

--- pages/profile/thumbs.json.php
+++ pages/profile/thumbs.json.php
@@ -1,39 +1,41 @@
 <?php
 ob_start('ob_gzhandler');
 
-if(isset($_POST['comment'])&&$_POST['comment']==true) {
-  require_once $_SERVER['DOCUMENT_ROOT'].'/class/comments.class.php';
-  $core = new comments();
-  if(!isset($_POST['hcid']) || !is_numeric($_POST['hcid'])) 
-    die($core->jsonResponse('error_no_hcid',$core->lang('ERROR')));
-  $id = $_POST['hcid'];
-} else {
-  require_once $_SERVER['DOCUMENT_ROOT'].'/class/messages.class.php';
-  $core = new messages();
-  if(!isset($_POST['hpid']) || !is_numeric($_POST['hpid'])) 
-    die($core->jsonResponse('error_no_hpid',$core->lang('ERROR')));
-  $id = $_POST['hpid'];
+if(isset($_POST['comment']) && $_POST['comment']) {
+    require_once $_SERVER['DOCUMENT_ROOT'].'/class/comments.class.php';
+    $core = new comments();
+    if(!isset($_POST['hcid']) || !is_numeric($_POST['hcid'])) 
+        die($core->jsonResponse('error_no_hcid',$core->lang('ERROR')));
+    $id = $_POST['hcid'];
+}
+else
+{
+    require_once $_SERVER['DOCUMENT_ROOT'].'/class/messages.class.php';
+    $core = new messages();
+    if(!isset($_POST['hpid']) || !is_numeric($_POST['hpid'])) 
+        die($core->jsonResponse('error_no_hpid',$core->lang('ERROR')));
+    $id = $_POST['hpid'];
 }
 
 if (!$core->isLogged()) {
     die($core->jsonResponse('error',$core->lang('REGISTER')));
 }
 
-if (isset($_POST['thumb'])){
-  if(is_numeric($_POST['thumb'])) {
+if ( isset($_POST['thumb']) && is_numeric($_POST['thumb']) ) {    
     $thumb = (int) $_POST['thumb'];
 
     if ($thumb > 1 || $thumb < -1) {
-      die($core->jsonResponse('error1',$core->lang('ERROR')));
+        die($core->jsonResponse('error',$core->lang('ERROR').': 1'));
     }
 
     if (!$core->setThumbs($id, $thumb)) {
-      die($core->jsonResponse('error2',$core->lang('ERROR')));
+        die($core->jsonResponse('error',$core->lang('ERROR').': 2'));
     }
-  } else {
-    die($core->jsonResponse('error3',$core->lang('ERROR')));
-  }
+}   
+else {
+    die($core->jsonResponse('error',$core->lang('ERROR').': 3'));
 }
+
 
 die($core->jsonResponse('thumbs', $core->getThumbs($id)));
 
