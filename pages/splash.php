c59af767a4f1b3c188378df3ad5f18032eddc3a3
Dr.Jest <dc.jest@gmail.com>
2014-03-26 03:05:21 +0100

    Fixed Mobile Splash, update tpl/0-1-2

--- pages/splash.php
+++ pages/splash.php
@@ -1,13 +1,8 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/pages/common/vars.php';
 $vals['mobile_b'] = $core->isMobile();
-if(!$core->isMobile()) {
-  $vals['yes_link_n'] = 'mobile';
-  $vals['nop_link_n'] = 'desktop';
-} else {
-  $vals['yes_link_n'] = 'desktop';
-  $vals['nop_link_n'] = 'mobile';
-}
+$vals['gotomobile_n'] = $_SERVER['REQUEST_URI']."&goto=mobile";
+$vals['gotodesktop_n'] = $_SERVER['REQUEST_URI']."&goto=desktop";
 $core->getTPL()->assign($vals);
 $core->getTPL()->draw('base/splash');
 ?>
