916db5b8047920ede5e74abfd7a6a910e7e2c7c4
Twentyone <gio.balduz@gmail.com>
2014-03-27 13:25:57 +0100

    fixed postgres_schema.sql

--- setup/postgres_schema.sql
+++ setup/postgres_schema.sql
@@ -51,6 +51,7 @@ CREATE TABLE profiles (
   yahoo varchar(350) NOT NULL DEFAULT '',
   userscript varchar(128) NOT NULL DEFAULT '',
   template int2 NOT NULL DEFAULT 0,
+  mobile_template int2 NOT NULL DEFAULT 1,
   dateformat varchar(25) NOT NULL DEFAULT 'd/m/Y, H:i',
   facebook varchar(350) NOT NULL DEFAULT '',
   twitter varchar(350) NOT NULL DEFAULT '',
@@ -306,7 +307,6 @@ CREATE TABLE groups_thumbs (
   CONSTRAINT userGThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
   CONSTRAINT chkGVote CHECK("vote" IN (-1, 0, 1))
 );
-
 
 
 CREATE TABLE groups_lurkers (
@@ -576,6 +576,30 @@ CREATE TRIGGER before_insert_on_lurkers BEFORE INSERT ON lurkers FOR EACH ROW EX
 
 --END before_insert_on_lurkers
 
+--BEGIN comment thumbs
+
+CREATE TABLE comment_thumbs (
+  "hcid" int8 NOT NULL,
+  "user" int8 NOT NULL,
+  "vote" int2 NOT NULL,
+  PRIMARY KEY("hcid", "user"),
+  CONSTRAINT hcidThumbs FOREIGN KEY ("hcid") REFERENCES comments(hcid) ON DELETE CASCADE,
+  CONSTRAINT userThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
+  CONSTRAINT chkVote CHECK("vote" IN (-1, 0, 1))
+);
+
+CREATE TABLE comment_groups_thumbs (
+  "hcid" int8 NOT NULL,
+  "user" int8 NOT NULL,
+  "vote" int2 NOT NULL,
+  PRIMARY KEY("hcid", "user"),
+  CONSTRAINT hcidGThumbs FOREIGN KEY ("hcid") REFERENCES groups_comments(hcid) ON DELETE CASCADE,
+  CONSTRAINT userGThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
+  CONSTRAINT chkGVote CHECK("vote" IN (-1, 0, 1))
+);
+
+--END comment thumbs
+
 --BEGIN before_delete_post
 
 CREATE FUNCTION before_delete_post() RETURNS TRIGGER AS $func$
@@ -603,30 +627,6 @@ $func$ LANGUAGE plpgsql;
 CREATE TRIGGER before_delete_post BEFORE DELETE ON posts FOR EACH ROW EXECUTE PROCEDURE before_delete_post();
 
 --END before_delete_post
-
---BEGIN comment_thumbs
-
-CREATE TABLE comment_thumbs (
-  "hcid" int8 NOT NULL,
-  "user" int8 NOT NULL,
-  "vote" int2 NOT NULL,
-  PRIMARY KEY("hcid", "user"),
-  CONSTRAINT hcidThumbs FOREIGN KEY ("hcid") REFERENCES comments(hcid) ON DELETE CASCADE,
-  CONSTRAINT userThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
-  CONSTRAINT chkVote CHECK("vote" IN (-1, 0, 1))
-);
-
-CREATE TABLE comment_groups_thumbs (
-  "hcid" int8 NOT NULL,
-  "user" int8 NOT NULL,
-  "vote" int2 NOT NULL,
-  PRIMARY KEY("hcid", "user"),
-  CONSTRAINT hcidGThumbs FOREIGN KEY ("hcid") REFERENCES groups_comments(hcid) ON DELETE CASCADE,
-  CONSTRAINT userGThumbs FOREIGN KEY ("user") REFERENCES users(counter) ON DELETE CASCADE,
-  CONSTRAINT chkGVote CHECK("vote" IN (-1, 0, 1))
-);
-
---END comment_thumbs
 
 --BEGIN before_delete_user
 
@@ -688,4 +688,3 @@ CREATE TRIGGER before_delete_user BEFORE DELETE ON users FOR EACH ROW EXECUTE PR
 
 --Commit
 COMMIT;
-
