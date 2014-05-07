8ae869926efb322958e4a3ad0dec6468f90b7516
nerdzeu <nessuno@nerdz.eu>
2014-05-07 17:32:47 +0200

    Improve init_postgres.sh

--- setup/init_postgres.sh
+++ setup/init_postgres.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-set -e ;
+start_progress () {
+    while true; do
+        echo -ne "|\b"
+        sleep 0.1
+        echo -ne "/\b"
+        sleep 0.1
+        echo -ne "-\b"
+        sleep 0.1
+    done
+}
 
 if [ $# -lt 2 ]; then
     echo "Usage: $0 existingRole db&username"
     echo "Example: $0 postgres nerdz"
     echo "    Existing role postgres will create a new database named nerdz and a new user with the same name"
+    exit -1
+fi
+
+if [ ! -f postgres_schema.sql ]; then
+    echo "No postgres_schema.sql found in current PWD.\n (Are you in NERDZ_ROOT/setup/ ?)" 1>&2
     exit -1
 fi
 
@@ -15,12 +29,11 @@ dropdb   -U "$1" "$2" &> /dev/null || true
 dropuser -U "$1" "$2" &> /dev/null || true
 
 echo "Done." ; echo
-echo  "Creating database and user $2 (you'll be asked for password)..."
+
+echo  "Creating database and user: $2 (you'll be asked for password)..."
 
 createuser -P -U "$1" -S "$2" || exit -1
 createdb -U "$1" "$2" || exit -1
-
-echo "Done." ; echo
 
 echo -n "Setting variables and privileges..."
 
@@ -33,15 +46,15 @@ CREATE EXTENSION pgcrypto\;
 EOF
 
 echo "Done." ; echo
-echo -n "Loading nerdz database schema and triggers into PostgreSQL... "
+echo -n "Loading nerdz database schema and triggers into $2 ... "
 
-if test -f postgres_schema.sql ; then
-    psql -U "$1" "$2" < postgres_schema.sql 1>/dev/null
-else
-    echo "\nNo postgres_schema.sql found in current PWD.\n (Are you in NERDZ_ROOT/setup/ ?)" 1>&2
-    exit -1
-fi
+start_progress &
+PROGRESS_PID=$!
+psql -U "$1" "$2" < postgres_schema.sql 1> /dev/null
+disown $PROGRESS_PID
+kill $PROGRESS_PID
 
-echo "Done." ; echo
-echo "REMEMBER TO SET timezone = 'UTC' IN postgresql.conf OR NOTHING WILL WORK!"
+echo  "Done"; echo
 echo "If you have an existing MySQL database, you can now migrate it with setup/nerdz_my2pg.groovy."
+
+exit 0
