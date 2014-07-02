1d5b960e1602f73c6ef29ee5a1cccd957a9bbee9
Paolo Galeone <nessuno@nerdz.eu>
2014-07-02 20:25:57 +0200

    Remove dependency from external C executable. Stats from PHP

--- pages/stats.php
+++ pages/stats.php
@@ -1,28 +1,54 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/class/utils.class.php';
+
 $utils = new utils();
+
 $ret = array();
 $vals = array();
 
 $cache = 'nerdz_stats'.SITE_HOST;
 
+function createArray(&$core, &$ret, $query, $position) {
+
+    if(!($o = $core->query($query, db::FETCH_OBJ)))
+        $ret[$position] = -1;
+    else
+        $ret[$position] = $o->cc;
+}
+
 if(apc_exists('nerdz_stats'))
     $ret = unserialize(apc_fetch('nerdz_stats'));
 else
 {
-    exec($_SERVER['DOCUMENT_ROOT'].'/pages/executables/site_stats.ccgi',$ret); //ret[0,6]
-    @apc_store($cache,serialize($ret),300); //5 min
+    require_once $_SERVER['DOCUMENT_ROOT'].'/class/core.class.php';
+    $core = new phpCore();
+    $ret = [];
+
+    $queries = [
+        0 => 'SELECT COUNT(counter) AS cc FROM users',
+        1 => 'SELECT COUNT(hpid)    AS cc FROM posts',
+        2 => 'SELECT COUNT(hcid)    AS cc FROM comments',
+        3 => 'SELECT COUNT(counter) AS cc FROM groups',
+        4 => 'SELECT COUNT(hpid)    AS cc FROM groups_posts',
+        5 => 'SELECT COUNT(hcid)    AS cc FROM groups_comments',
+        6 => 'SELECT COUNT(counter) AS cc FROM users WHERE last > (NOW() - INTERVAL \'4 MINUTES\') AND viewonline IS TRUE',
+        7 => 'SELECT COUNT(counter) AS cc FROM users WHERE last > (NOW() - INTERVAL \'4 MINUTES\') AND viewonline IS FALSE',
+    ];
+    foreach($queries as $position => $query) {
+        createArray($core, $ret, $query, $position);
+    }
+    @apc_store($cache,serialize($ret),900); //15 min
 }
 
-$vals['totusers_n'] = $ret[0];
-$vals['totpostsprofiles_n'] = $ret[1];
-$vals['totcommentsprofiles_n'] = $ret[2];
-$vals['totprojects_n'] = $ret[3];
-$vals['totpostsprojects_n'] = $ret[4];
-$vals['totcommentsprojects_n'] = $ret[5];
-$vals['totonlineusers_n'] = $ret[6];
-$vals['tothiddenusers_n'] = $ret[7];
-$vals['lastupdate_n'] = $core->getDateTime($utils->apc_getLastModified($cache));
+$vals['totusers_n']             = $ret[0];
+$vals['totpostsprofiles_n']     = $ret[1];
+$vals['totcommentsprofiles_n']  = $ret[2];
+$vals['totprojects_n']          = $ret[3];
+$vals['totpostsprojects_n']     = $ret[4];
+$vals['totcommentsprojects_n']  = $ret[5];
+$vals['totonlineusers_n']       = $ret[6];
+$vals['tothiddenusers_n']       = $ret[7];
+$vals['lastupdate_n']           = $core->getDateTime($utils->apc_getLastModified($cache));
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/pages/common/vars.php';
 
