e154da32b99360f00aa6728ab134f8e848232383
Paolo Galeone <nessuno@nerdz.eu>
2014-09-10 14:03:11 +0200

    Add trends to cache

--- pages/search.php
+++ pages/search.php
@@ -1,12 +1,22 @@
 <?php
 use NERDZ\Core\Trend;
+use NERDZ\Core\Utils;
+use NERDZ\Core\Config;
 
 $vals = [];
 $vals['querystring_n'] = $q;
-$trend = new Trend();
+$cache = 'trends'.Config\SITE_HOST;
+if(!($trends = Utils::apc_get($cache)))
+    $trends = Utils::apc_set($cache, function() {
+        $trend = new Trend();
+        $ret = [];
+        $ret['popular'] = $trend->getPopular();
+        $ret['newest']  = $trend->getNewest();
+        return $ret;
+    },300);
 
-$vals['popular_a'] = $trend->getPopular();
-$vals['newest_a']  = $trend->getNewest();
+$vals['popular_a'] = $trends['popular'];
+$vals['newest_a']  = $trends['newest'];
 
 $user->getTPL()->assign($vals);
 $user->getTPL()->draw('search/layout');
