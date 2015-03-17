f79edfdacf44602be04cba0acb9735781992416c
Paolo Galeone <nessuno@nerdz.eu>
2015-03-17 11:50:26 +0100

    Handle jclass.js modifications in temp file

--- static/js/api.php
+++ static/js/api.php
@@ -7,17 +7,25 @@ use NERDZ\Core\Minification;
 use NERDZ\Core\Config;
 
 $filename = $_SERVER['DOCUMENT_ROOT'].'/static/js/jclass.js';
-$tmpFile = $_SERVER['DOCUMENT_ROOT'].'/tmp/japi.js';
+$tmpFile  = $_SERVER['DOCUMENT_ROOT'].'/tmp/japi.js';
+$tmpFileTime = $_SERVER['DOCUMENT_ROOT'].'/tmp/japi.js.time';
+
+$updateTime = 0;
+
+if(!file_exists ($tmpFileTime) ||
+    intval(file_get_contents($tmpFileTime)) < ($updateTime = filemtime($filename))) {
+    $content = Minification::minifyJs($filename);
+    file_put_contents($tmpFile, $content);
+    file_put_contents($tmpFileTime, $updateTime);
+    chmod($tmpFile, 0775);
+    chmod($tmpFileTime, 0775);
+}
+
+$content = file_get_contents($tmpFile);
 
 header('Content-type: application/javascript');
 header('Vary: Accept-Encoding');
 header('Etag: '.md5_file($filename));
 
-if(is_readable($tmpFile))
-    $content = file_get_contents($tmpFile);
-else {
-    $content = Minification::minifyJs($filename);
-    file_put_contents($tmpFile, $content);
-    chmod ($tmpFile, 0775);
-}
+
 die($content);
