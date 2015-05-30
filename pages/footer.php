ebd5c084522388cd6471fa745dddae3a50ee169b
Paolo Galeone <nessuno@nerdz.eu>
2015-05-30 12:03:53 +0200

    Add cookie law notice. Update submodules

--- pages/footer.php
+++ pages/footer.php
@@ -27,3 +27,18 @@ if(in_array($_SERVER['SCRIPT_NAME'], array('/profile.php','/project.php'))) { ?>
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gi,s);
 })();
 </script>
+<?php
+if(!isset($_COOKIE['stupid_and_useless_cookielaw'])) { ?>
+<div id="cookieChoiceInfo" style="position: fixed; width: 100%; border-top-width: 1px; border-top-style: solid; border-top-color: rgb(204, 204, 204); color: rgb(119, 119, 119); font-size: 12px; margin: 0px; left: 0px; bottom: 0px; padding: 10px 0px; z-index: 1000; text-align: center; background-color: rgb(230, 230, 230);">
+    <span><?php echo $user->lang('COOKIE_LAW_NOTICE') ?></span>
+    <a href="/terms.php#cookiePolicy" target="_blank" style="color: rgb(119, 119, 119); text-decoration: underline; margin-left: 20px;"><?php echo $user->lang('INFORMATIONS') ?></a>
+    <a id="cookieChoiceDismiss" href="#" style="color: rgb(255, 255, 255); padding: 3px; margin-left: 20px; background-color: rgb(255, 102, 0);">OK</a>
+</div>
+    <script>
+    $("#cookieChoiceDismiss").on('click',function(e) {
+        e.preventDefault();
+        $("#cookieChoiceInfo").remove();
+        document.cookie = "stupid_and_useless_cookielaw=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+    });
+    </script>
+<?php } ?>
