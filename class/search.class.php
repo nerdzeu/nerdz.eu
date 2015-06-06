45c11da5d963c9b3416738cec6b4b3d3bfbd1cfd
Paolo Galeone <nessuno@nerdz.eu>
2015-06-06 13:53:13 +0200

    Move hashtag search (now topic search) to search class. Rename search username/projectname methods

--- class/search.class.php
+++ class/search.class.php
@@ -11,6 +11,7 @@ class Search {
     public function __construct() {
         $this->user = new User();
         $this->project = new Project();
+        $this->messages = new Messages();
     }    
 
     private function byName($contains, $limit, $project = false) {
@@ -44,11 +45,69 @@ class Search {
         return $ret;
     }
 
-    public function Username($contains, $limit) {
+    public function username($contains, $limit) {
         return $this->byName($contains, $limit);
     }
 
-    public function ProjectName($contains, $limit) {
+    public function projectName($contains, $limit) {
         return $this->byName($contains, $limit, true);
     }
+
+    public function topic($tag, $limit, $hpid = 0) {
+        $imp_blist = implode(',',$this->user->getBlacklist());
+
+        $query = 'with tagged_posts(u_hpid, g_hpid) as (
+            select u_hpid, g_hpid from posts_classification
+            where lower(tag) = lower(:tag)) ';
+
+        $query.= ' SELECT * FROM ( (SELECT p.hpid, p.from, p.to, p.closed, p.lang, p.news, EXTRACT(EPOCH FROM p."time") AS time, p.message, p.pid, false as "group"
+                    FROM posts p INNER JOIN (select u_hpid FROM tagged_posts) AS pc
+                    ON pc.u_hpid = p.hpid ';
+
+        if(!empty($imp_blist))
+            $query .= ' WHERE p."from" NOT IN ('.$imp_blist.') AND p."to" NOT IN ('.$imp_blist.') ';
+
+        $query.= ') union (
+                   SELECT gp.hpid, gp.from, gp.to, gp.closed, gp.lang, gp.news, EXTRACT(EPOCH FROM gp."time") AS time, gp.message, gp.pid, true as "group"
+                    FROM "groups_posts" gp INNER JOIN (select g_hpid FROM tagged_posts) AS pc
+                    ON pc.g_hpid = gp.hpid ';
+
+        if(!empty($imp_blist))
+            $query .= ' WHERE gp."from" NOT IN ('.$imp_blist.')';
+        else {
+            $query .= ' WHERE TRUE';
+        }
+
+        $query.= ' AND gp.to NOT IN (SELECT counter FROM groups WHERE private IS TRUE) )) AS t ';
+
+        if($hpid) {
+            $query .= ' WHERE t.hpid < :hpid ';
+        }
+        $query.= ' ORDER BY t.time DESC LIMIT '.$limit;
+
+        if(!($result = Db::query(
+            [
+                $query,
+                array_merge(
+                    [ ':tag' => $tag ],
+                    $hpid ? [ ':hpid' => $hpid ] : []
+                )
+            ],Db::FETCH_STMT))
+        )
+        return [];
+
+        $c = 0;
+        $ret = [];
+        while(($row = $result->fetch(PDO::FETCH_OBJ)))
+        {
+            $ret[$c] = $this->messages->getPost($row,
+                [
+                    'project'  => $row->group,
+                    'truncate' => true
+                ]);
+            ++$c;
+        }
+        return $ret;
+    }
+
 }
