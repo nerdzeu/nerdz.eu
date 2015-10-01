3255f9e5b4e47766d5754da8259aec5ace6f760b
Paolo Galeone <nessuno@nerdz.eu>
2015-10-01 15:06:17 +0200

    Remove fixed background color. Wrap in nerdz-code-wrapper

--- class/messages.class.php
+++ class/messages.class.php
@@ -927,10 +927,10 @@ class Messages
 
             if($tag == 'c') { // short
                 $str = str_ireplace("[c={$lang}]{$totalcode}[/c]",
-                    '<code class="prettyprint lang-'.$lang.'" style="border:0px; word-wrap: break-word; background-color:#eee; color:#000">'.
+                    '<span class="nerdz-code-wrapper"><code class="prettyprint lang-'.$lang.'" style="border:0px; word-wrap: break-word">'.
                     str_replace("\n",'<br />',
                         str_replace(' ','&nbsp',
-                        str_replace("\t",'&#09;',$totalcode))).'</code>', $str);
+                        str_replace("\t",'&#09;',$totalcode))).'</code></span>', $str);
             } else { // long
                 $str = str_ireplace("[code={$lang}]{$totalcode}[/code]",
                     '<div class="nerdz-code-wrapper">
