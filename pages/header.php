aed33b957547e2dc67f9fbbfad4a2beb902934dc
Paolo Galeone <nessuno@nerdz.eu>
2015-11-02 21:43:42 +0100

    Add position_n variable to header

--- pages/header.php
+++ pages/header.php
@@ -1,3 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/pages/common/vars.php';
+$vars = [];
+$positionArray = explode('.',basename($_SERVER['PHP_SELF']));
+$vars['position_n'] = reset($positionArray);
+$user->getTPL()->assign($vars);
 $user->getTPL()->draw('base/header');
