f706bb803d2bbadd50bea2ab149037ac2ad5c53c
Paolo Galeone <nessuno@nerdz.eu>
2016-02-29 13:29:53 +0100

    Fix typo

--- class/pms.class.php
+++ class/pms.class.php
@@ -72,7 +72,7 @@ final class Pms extends Messages
             if(!in_array($o->from, $froms)) {
                 $from = User::getUsername($o->from);
                 $res[$c]['from4link_n'] = \NERDZ\Core\Utils::userLink($from);
-                $res[$c]['fromgravatarurl_n'] = $user->user->getGravatar($o->from);
+                $res[$c]['fromgravatarurl_n'] = $this->user->getGravatar($o->from);
                 $res[$c]['fromid_n'] = $o->from;
                 $res[$c]['from_n'] = $from;
                 $res[$c]['datetime_n'] = $this->user->getDateTime($o->lasttime);
