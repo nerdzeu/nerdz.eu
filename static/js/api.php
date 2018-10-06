f65c236b58a1c4ce585332e82d1017a4daf4e2ce
Roberto Frenna <roberto.frenna@gmail.com>
2018-10-06 20:40:53 +0200

    api: short circuit if minification is disabled
    This prevents an unnecessary temporary file.

--- static/js/api.php
+++ static/js/api.php
@@ -7,23 +7,27 @@ use NERDZ\Core\Minification;
 use NERDZ\Core\Config;
 
 $filename = $_SERVER['DOCUMENT_ROOT'].'/static/js/jclass.js';
-$tmpFile  = $_SERVER['DOCUMENT_ROOT'].'/tmp/japi.js';
-$tmpFileTime = $_SERVER['DOCUMENT_ROOT'].'/tmp/japi.js.time';
+$content = '';
 
-$updateTime = 0;
+if (Config\MINIFICATION_ENABLED) {
+    $tmpFile  = $_SERVER['DOCUMENT_ROOT'].'/tmp/japi.js';
+    $tmpFileTime = $_SERVER['DOCUMENT_ROOT'].'/tmp/japi.js.time';
 
-if(!file_exists ($tmpFileTime) ||
-    intval(file_get_contents($tmpFileTime)) < ($updateTime = filemtime($filename))) {
-    $content = Config\MINIFICATION_ENABLED
-        ? Minification::minifyJs ($filename)
-        : file_get_contents($filename);
-    file_put_contents($tmpFile, $content);
-    file_put_contents($tmpFileTime, $updateTime);
-    chmod($tmpFile, 0775);
-    chmod($tmpFileTime, 0775);
+    $updateTime = 0;
+
+    if(!file_exists ($tmpFileTime) ||
+        intval(file_get_contents($tmpFileTime)) < ($updateTime = filemtime($filename))) {
+        $content = Minification::minifyJs($filename);
+        file_put_contents($tmpFile, $content);
+        file_put_contents($tmpFileTime, $updateTime);
+        chmod($tmpFile, 0775);
+        chmod($tmpFileTime, 0775);
+    }
+
+    $content = file_get_contents($tmpFile);
+} else {
+    $content = file_get_contents($filename);
 }
-
-$content = file_get_contents($tmpFile);
 
 header('Content-type: application/javascript');
 header('Vary: Accept-Encoding');
