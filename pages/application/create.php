9ab97f150d44fdb6c4c6dcb8e27b5bad99a55dee
Paolo Galeone <nessuno@nerdz.eu>
2020-04-13 21:10:32 +0200

    fix format

--- pages/application/create.php
+++ pages/application/create.php
@@ -3,8 +3,8 @@
 use NERDZ\Core\OAuth2;
 
 $scopes = [];
-foreach(OAuth2::SCOPES as $scope) {
-    foreach(OAuth2::PERMISSIONS as $permission) {
+foreach (OAuth2::SCOPES as $scope) {
+    foreach (OAuth2::PERMISSIONS as $permission) {
         $scopes[] = strtolower("${scope}:${permission}");
     }
 }
@@ -12,7 +12,7 @@ $scopeStr = implode(' ', $scopes);
 $scope_descr = array_map([$user, 'lang'], OAuth2::getScopes($scopeStr));
 $vals['scopes_a'] = [];
 $len = count($scopes);
-for($i=0;$i<$len;++$i) {
+for ($i=0;$i<$len;++$i) {
     $scope = $scopes[$i];
     $vals["scopes_a"][$scope] = $scope_descr[$i];
 }
